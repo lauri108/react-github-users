29efe23d843be208bcca2adc857cc208aba97535
= <=>
2020-08-05 19:45:50 +0800

    parsing and outputting API response

--- src/components/DragDropArea.js
+++ src/components/DragDropArea.js
@@ -1,6 +1,36 @@
 import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 export default class DragDropArea extends Component {
+  constructor() {
+    super();
+    this.state = {
+      CSVResults: [],
+    };
+    //this.CSVResults = [];
+  }
   onDragOver = (event) => {
     event.preventDefault();
     event.stopPropagation();
@@ -25,6 +55,10 @@ export default class DragDropArea extends Component {
     [...files].forEach(this.uploadFile);
   };
   processUploadResult = (result) => {
+    //debugger;
+    this.setState({
+      CSVResults: [...result.parsedItems],
+    });
     console.log(`upload success, here's the data: `, result);
   };
   processUploadError = (error) => {
@@ -34,15 +68,15 @@ export default class DragDropArea extends Component {
     const url = "http://localhost:8000/api/import";
     let formData = new FormData();
 
-    formData.append("name_of_file_here", file);
+    formData.append("bom", file);
 
     fetch(url, {
       method: "POST",
       body: formData,
     })
-      .then((result) => {
+      .then(async (result) => {
         /* Done. Inform the user */
-        this.processUploadResult(result);
+        this.processUploadResult(await result.json());
       })
       .catch((error) => {
         /* Error. Inform the user */
@@ -51,15 +85,28 @@ export default class DragDropArea extends Component {
   };
   render() {
     return (
-      <div
-        className="droppable w-full max-w-sm mx-auto pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900"
-        id="dropArea"
-        onDragOver={(e) => this.onDragOver(e)}
-        onDragEnter={(e) => this.onDragEnter(e, "complete")}
-        onDrop={(e) => this.onDrop(e, "complete")}
-      >
-        {" "}
-        Drop here
+      <div>
+        <Card className="ui-test">
+          <CardContent>
+            <div
+              className="droppable w-full max-w-sm mx-auto pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900"
+              id="dropArea"
+              onDragOver={(e) => this.onDragOver(e)}
+              onDragEnter={(e) => this.onDragEnter(e, "complete")}
+              onDrop={(e) => this.onDrop(e, "complete")}
+            >
+              {" "}
+              Drop here
+            </div>
+          </CardContent>
+        </Card>
+        {this.state.CSVResults.map((item) => (
+          <Card className="ui-test">
+            <CardContent>
+              name: {item.name}, qty: {item.quantity}
+            </CardContent>
+          </Card>
+        ))}
       </div>
     );
   }
